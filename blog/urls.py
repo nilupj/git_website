88b4bfdb0694fd56d88c4eb399a46d65588eac8f
nirvay behara <102511177+nilupj@users.noreply.github.com>
2025-01-22 23:50:19 +0530

    Update urls.py

--- blog/urls.py
+++ blog/urls.py
@@ -7,6 +7,5 @@ from . import views
 
 urlpatterns = [
     path('health-topics/', views.health_topics, name='health_topics'),
-    path('topic/<int:pk>/', views.TopicDetailView.as_view(), name='topic_detail')
-    path('', views.home, name='home'),
+    path('topic/<int:pk>/', views.TopicDetailView.as_view(), name='topic_detail'),
 ]
