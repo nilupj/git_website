e3177609e5f7af9042ec7db7e7c0f9a106ac5292
nirvay behara <102511177+nilupj@users.noreply.github.com>
2025-01-23 12:41:50 +0530

    Update dev.py

--- medical_site/settings/dev.py
+++ medical_site/settings/dev.py
@@ -7,7 +7,7 @@ DEBUG = False
 SECRET_KEY = "django-insecure-^!9+@&l4d*w^^%i=k-i+ie7)$gywj+^!=*vs*s81yuj0hg5^!_"
 
 # SECURITY WARNING: define the correct hosts in production!
-ALLOWED_HOSTS = (['niinfomed.onrender.com'])
+ALLOWED_HOSTS = (['git-website.onrender.com'])
 #ALLOWED_HOSTS = ["*"]
 EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 
